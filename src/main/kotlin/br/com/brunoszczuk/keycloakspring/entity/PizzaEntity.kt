7cf1ee08b287a9e008b1c567d632f25796cbbac9
Bruno Szczuk <brunoszczuk@gmail.com>
2023-09-05 13:11:51 -0300

    step back

--- src/main/kotlin/br/com/brunoszczuk/keycloakspring/entity/PizzaEntity.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brunoszczuk.keycloakspring.entity
-
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-
-@Entity
-data class PizzaEntity(@Id val id: Long, val name: String)
--- src/main/kotlin/br/com/brunoszczuk/keycloakspring/repository/PizzaRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brunoszczuk.keycloakspring.repository
-
-import br.com.brunoszczuk.keycloakspring.entity.PizzaEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PizzaRepository : JpaRepository<PizzaEntity, Long> {
-}
