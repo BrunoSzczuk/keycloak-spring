3c604baf3b7097da88e5de22160c683c7eb7c246
Bruno Szczuk <brunoszczuk@gmail.com>
2023-09-05 13:22:05 -0300

    step back

--- src/main/kotlin/br/com/brunoszczuk/keycloakspring/KeycloakSpringApplication.kt
+++ src/main/kotlin/br/com/brunoszczuk/keycloakspring/KeycloakSpringApplication.kt
@@ -2,12 +2,8 @@ package br.com.brunoszczuk.keycloakspring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
-@EnableWebSecurity
-@EnableMethodSecurity
 class KeycloakSpringApplication
 
 fun main(args: Array<String>) {
